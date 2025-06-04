41dd781cad57e25c8b315152a3c3f85b40658aba
xzh <zhilang161@gmail.com>
2025-06-04 22:10:54 +0800

    feat: update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.jpeg" sizes="any" />
+      </Head>
+
       <body className="antialiased">
         <Main />
         <NextScript />
